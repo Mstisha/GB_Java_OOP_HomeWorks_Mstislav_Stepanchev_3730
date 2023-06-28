7bfd4a18c53d32735fa9b4e1815ec5f2cd625cb9
Slava <mistik99@mail.ru>
2023-06-28 18:48:39 +0200

    HomeWork

---
+++ Observer.java
@@ -0,0 +1,4 @@
+public interface Observer {
+
+    void receiveOffer(String nameCompany, double salary, Profession vacancyProfession);
+}
---
+++ Profession.java
@@ -0,0 +1,7 @@
+public enum Profession {
+    Programmist,
+    Tester,
+    Analyst,
+    WebDesigner,
+    ProjectManager
+}
